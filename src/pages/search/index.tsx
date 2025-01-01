10535967891350bedabc202264631ab79d8cb646
Sang-Ho Jeon <67211899+Sang-Ho-Jeon@users.noreply.github.com>
2025-01-01 23:25:25 +0900

    feat: add seo setting

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -4,6 +4,7 @@ import BookItem from "@/components/book-item";
 import fetchBooks from "@/lib/fetch-books";
 import { useRouter } from "next/router";
 import { BookData } from "@/type";
+import Head from "next/head";
 
 // export const getStaticProps = async (context : GetStaticPropsContext) => {
 //   const q = context.query.q;
@@ -34,11 +35,19 @@ export default function Page () {
     }
   }, [q])
 
-  return <div>
-    {books.map((book) => 
-      <BookItem key={book.id} {...book}/>
-    )}
-  </div>
+  return (
+    <div>
+      <Head>
+        <title>한입북스 - 검색결과</title>
+        <meta property="og:image" content="/thumbnail.png"/>
+        <meta property="og:title" content="한입북스 - 검색결과"/>
+        <meta property="og:description" content="한입 북스에 등록된 도서들을 만나보세요"/>
+      </Head>
+      {books.map((book) => 
+        <BookItem key={book.id} {...book}/>
+      )}
+    </div>
+  )
 }
 
 Page.getLayout = (page: ReactNode) => {
