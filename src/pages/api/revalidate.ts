f74649e248bd33830174255a3fd83c2daa979e33
Sang-Ho Jeon <67211899+Sang-Ho-Jeon@users.noreply.github.com>
2025-01-01 22:46:07 +0900

    feat: practice ISR

---
+++ src/pages/api/revalidate.ts
@@ -0,0 +1,14 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function hanlder(
+  req:NextApiRequest,
+  res:NextApiResponse
+) {
+  try{
+    await res.revalidate('/')
+    return res.json({ revalidate: true });
+  } catch(err) {
+    res.status(500).send("Revalidation Failed");
+    console.log(err);
+  }
+}
